17f6f8ac0440047976f98ec9c39564915cb1f5a6
Penifana Ouattara <penifana.ouattara@cellenza.com>
2023-12-20 13:25:15 +0100

    - ajout model dans le domaine - ajout dependance de wepApi à l'infra

---
+++ Insurance.Auto/Insurance.Auto.Domain/Models/ContratAuto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance.Auto.Domain.Models
+{
+    internal class ContratAuto
+    {
+        public string Numero { get; set; }
+        public DateTime DateSouscription { get;set; }
+        public DateTime DateDePriseEffet { get; set; }
+        public DateTime DateResiliation { get;set; }
+        public Souscripteur Souscripteur { get; set; }
+        public List<Voiture> VoituresAssurees { get; set;}
+    }
+}
